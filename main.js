de5ab2a56c64d7766209170c2ec321ec5d01a1e0
Karim <konayq@gmail.com>
2019-08-29 15:48:15 +0300

    df

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function make_base() {
         context.font = 'bold 64px Tajawal';
         context.textAlign = 'center';
         context.fillStyle = "#8cc344";
-        context.fillText(textInput, 640, 600);
+        context.fillText(textInput, 640, 630);
 
         document.getElementById("theimage").src = canvas.toDataURL();
 
