59a98f67c434fed71ab09b2e2a25ed16ad316ac4
Karim <konayq@gmail.com>
2019-08-29 15:59:09 +0300

    dear

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function make_base() {
         context.font = 'bold 64px Tajawal';
         context.textAlign = 'center';
         context.fillStyle = "#8cc344";
-        context.fillText(textInput, 640, 630);
+        context.fillText("Dear" + textInput+",", 640, 630);
 
         document.getElementById("theimage").src = canvas.toDataURL();
 
