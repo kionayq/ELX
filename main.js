0ca899a6a03de812e5fa9aaac4e7ce4d370fde42
karim <karim@MacBook-Pro.local>
2020-07-31 02:21:26 +0300

    f

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function make_base() {
         context.font = 'bold 44px Tajawal';
         context.textAlign = 'center';
         context.fillStyle = "#123251";
-        context.fillText(" " + textInput, 526, 930);
+        context.fillText(" " + textInput, 540, 930);
 
         document.getElementById("theimage").src = canvas.toDataURL();
 
